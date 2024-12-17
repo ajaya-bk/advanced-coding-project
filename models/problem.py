30c71923186cd003c49efb381234bb3178b41118
ajaya-bk <avashsd@gmail.com>
2024-12-18 00:41:03 +0100

    Add files via upload

---
+++ models/problem.py
@@ -0,0 +1,19 @@
+from models.database import Database
+
+class Problem:
+    def __init__(self, title, description, location, reporter):
+        self.title = title
+        self.description = description
+        self.location = location
+        self.reporter = reporter
+        self.db = Database() 
+
+    def save(self):
+        query = "INSERT INTO problems (title, description, location, reporter) VALUES (?, ?, ?, ?)"
+        self.db.execute_query(query, (self.title, self.description, self.location, self.reporter))
+
+    @staticmethod
+    def fetch_all():
+        db = Database()
+        query = "SELECT * FROM problems"
+        return db.fetch_all(query)
